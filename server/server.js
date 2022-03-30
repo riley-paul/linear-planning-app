66d9a1fc48b466d46139df22e084d1e612e3f84f
rjp301 <rileypaul96@gmail.com>
2022-03-29 22:17:59 -0600

    add server and dotenv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
+// load .env data into process.env
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.listen(PORT).then(console.log(`app is listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
