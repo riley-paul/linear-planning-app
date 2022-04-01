19f63d9920ce5449162cb45cf06b7e575ffad50b
rjp301 <rileypaul96@gmail.com>
2022-04-01 13:39:57 -0600

    move map to seperate component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,9 @@
-import mapboxgl from "mapbox-gl";
-import { useRef, useState, useEffect } from "react";
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicmpwMzAxIiwiYSI6ImNsMWdwdjF6YjFjN2EzZHBjeWl4MnUyeXUifQ.s-5QTZX7xeofw36iclsJZA";
+import Map from "./components/Map";
 
 export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
-
-  return <div className="App">
-    <div ref={mapContainer} className="map-container"></div>
-  </div>;
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
 }
