70e143aabe956e319459f30ac74997784b90fda3
Riley Paul <rileypaul96@gmail.com>
2022-07-13 08:27:10 -0600

    remove node_modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,10 @@ import Map from "./components/Map";
 import ElevationProfile from "./components/ElevationProfile";
 
 export default function App() {
-
-
   return (
     <div className="App" style={{ margin: "10px" }}>
       <ElevationProfile />
-      {/* <Map />   */}
+      <Map />
     </div>
   );
 }
