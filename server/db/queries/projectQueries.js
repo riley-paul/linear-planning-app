583919f9110d8afacf71a00998f9e7f9003df403
Riley Paul <rileypaul96@gmail.com>
2022-07-13 08:56:56 -0600

    add blank files for routes and queries

---
+++ server/db/queries/projectQueries.js
@@ -0,0 +1,5 @@
+const db = require("../index.js");
+
+// create a new project
+
+// fetch information about project
--- server/index.js
+++ server/index.js
@@ -1,16 +1,54 @@
+// load .env variables
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
+const morgan = require("morgan");
 
-const PORT = 3000;
+app.use(cors());
+app.use(express.static("data"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use(cors())
-app.use(express.static("data"));
-
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+
+// app.use(express.static("public"));
+
+// // Home page
+// // Warning: avoid creating more routes in this file!
+// // Separate them into separate routes files (see above).
+
+// app.get("/login", (req, res) => {
+//   console.log(process.env.USER_ID);
+//   res.cookie('userId', process.env.USER_ID);
+//   res.cookie('mapsAPIKey', process.env.API_KEY);
+//   res.cookie('viewState', 0);
+//   res.redirect("/");
+// });
+
+// // Separated Routes for each Resource
+// const usersRoutes = require("./routes/users");
+// const mapsRoutes = require("./routes/maps");
+// const pinsRoutes = require("./routes/pins");
+// const favouritesRoutes = require("./routes/favourites");
+// const map_editorsRoutes = require("./routes/map_editors");
+
+// // Mount all resource routes
+// app.use('/users', usersRoutes);
+// app.use('/maps', mapsRoutes);
+// app.use('/pins', pinsRoutes);
+// app.use('/favourites', favouritesRoutes);
+// app.use('/map_editors', map_editorsRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
