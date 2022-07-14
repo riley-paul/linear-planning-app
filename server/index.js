09d14224eb330c6424fae43d86ba62d31c566f6e
Riley Paul <rileypaul96@gmail.com>
2022-07-13 18:49:26 -0600

    remove environment and application files to keep things simple for now

--- server/index.js
+++ server/index.js
@@ -2,16 +2,11 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-const ENV = require("./environment")
-
-console.log(ENV)
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-
-const read = require("./utils/read")
 
 app.use(cors());
 app.use(express.static("data"));
@@ -21,42 +16,23 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
-
-
-// app.use(express.static("public"));
-
-// // Home page
-// // Warning: avoid creating more routes in this file!
-// // Separate them into separate routes files (see above).
-
-// app.get("/login", (req, res) => {
-//   console.log(process.env.USER_ID);
-//   res.cookie('userId', process.env.USER_ID);
-//   res.cookie('mapsAPIKey', process.env.API_KEY);
-//   res.cookie('viewState', 0);
-//   res.redirect("/");
-// });
+const db = require("./db");
 
 // Separated Routes for each Resource
 const centerlinesRoutes = require("./routes/centerlines");
 const projectsRoutes = require("./routes/projects");
 const takeoffsRoutes = require("./routes/takeoffs");
 
-
 /**
  * api routes examples:
  *  /api/projects/1/centerlines/1
  *  /api/projects/1/takeoffs/5
- * 
+ *
  *  /api/projects/1/centerlines/1/centerline
  *  /api/projects/1/centerlines/1/footprint
  *  /api/projects/1/centerlines/1/elevation
- * 
- *  */ 
+ *
+ *  */
 
 // Mount all resource routes
 // app.use("/users", usersRoutes);
@@ -65,6 +41,9 @@ const takeoffsRoutes = require("./routes/takeoffs");
 // app.use("/favourites", favouritesRoutes);
 // app.use("/map_editors", map_editorsRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}`);
-// });
+
+app.close = () => db.end();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
