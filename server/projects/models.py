898bde9867ce79ab3df96e350a9b3e570b9ddef3
Riley Paul <rileypaul96@gmail.com>
2022-08-02 14:31:56 -0700

    rebuild react app

--- server/projects/models.py
+++ server/projects/models.py
@@ -10,14 +10,21 @@ class Project(models.Model):
 class Centerline(models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
   name = models.CharField(max_length=256)
+  rev_date = models.DateField(auto_now=True)
   measure_col = models.CharField(max_length=256,default="measure")
   centerline = models.JSONField()
   chainage = models.JSONField()
   elevation = models.JSONField(blank=True)
   footprint = models.JSONField(blank=True)
 
+  def __str__(self):
+    return self.name
+
 class Takeoff(models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
   name = models.CharField(max_length=256)
   description = models.TextField(blank=True)
   data = models.JSONField()
+
+  def __str__(self):
+    return self.name
