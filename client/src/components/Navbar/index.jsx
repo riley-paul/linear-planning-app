7b0ac2dd4d4b963b2b38cb29373aaff4eb00bf86
Riley Paul <rileypaul96@gmail.com>
2022-08-06 10:40:22 -0700

    Add more elements to navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,38 +1,78 @@
-import {
-  AppBar,
-  Container,
-  Box,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import * as m from "@mui/material";
+import * as mi from "@mui/icons-material";
+import * as React from "react";
 
 import "./index.scss";
 
+const drawerWidth = 240;
+
 export default function NavBar(props) {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <Box>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Linear Project Planning
-            </Typography>
-            <Button color="inherit">login</Button>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </Box>
+    <m.Box sx={{ display: "flex" }}>
+      <m.CssBaseline />
+      <m.AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <m.Toolbar>
+          <m.IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <mi.Menu />
+          </m.IconButton>
+          <m.Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Linear Project Planning
+          </m.Typography>
+          <m.Box sx={{display: "flex", gap: "10px"}}>
+            <m.Button color="inherit">projects</m.Button>
+            <m.Button variant="outlined" color="inherit">login</m.Button>
+          </m.Box>
+        </m.Toolbar>
+      </m.AppBar>
+
+      <m.Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <m.Toolbar />
+        <m.Box sx={{ overflow: "auto" }}>
+          <m.List>
+            <m.ListItem>
+              <m.FormControl fullWidth>
+                <m.InputLabel id="demo-simple-select-label">Age</m.InputLabel>
+                <m.Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="Age"
+                  onChange={handleChange}
+                >
+                  <m.MenuItem value={10}>Ten</m.MenuItem>
+                  <m.MenuItem value={20}>Twenty</m.MenuItem>
+                  <m.MenuItem value={30}>Thirty</m.MenuItem>
+                </m.Select>
+              </m.FormControl>
+            </m.ListItem>
+          </m.List>
+        </m.Box>
+      </m.Drawer>
+    </m.Box>
   );
 }
